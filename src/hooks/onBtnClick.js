3e6bbee377db7e76f10a8fb00071cdc14cf826dc
Bitan Das <biggyansagar@gmail.com>
2024-09-06 21:52:03 +0530

    added pawn conversion function

--- src/hooks/onBtnClick.js
+++ src/hooks/onBtnClick.js
@@ -1,4 +1,9 @@
 
+import Queen from '../pieces/Queen'
+import Rook from '../pieces/Rook'
+import Bishop from '../pieces/Bishop'
+import Knight from '../pieces/Knight'
+
 function onBtnClick(
         e, selectedPiece, setSelectedPiece, 
         highlight, setHighlight, board, 
@@ -24,19 +29,43 @@ function onBtnClick(
             && board[selectedRow][selectedCol].symbol === 'K'
             && selectedRow === row
             && Math.abs(selectedCol - col) === 2
-            ) {
-                if (col === selectedCol + 2) {
-                    temp[row][col - 1] = temp[row][col + 1];
-                    temp[row][col + 1] = null;
-                }
-                if (col === selectedCol - 2) {
-                    temp[row][col + 1] = temp[row][col - 2];
-                    temp[row][col - 2] = null;
-                }
-            } else {
-                temp[row][col].row = row;
-                temp[row][col].col = col;
+        ) {
+            if (col === selectedCol + 2) {
+                temp[row][col - 1] = temp[row][col + 1];
+                temp[row][col + 1] = null;
             }
+            if (col === selectedCol - 2) {
+                temp[row][col + 1] = temp[row][col - 2];
+                temp[row][col - 2] = null;
+            }
+        } else if (
+            selectedPiece !== null
+            && board[selectedRow][selectedCol].symbol === 'P'
+            && (row === 0 || row === 7)
+        ) {
+            let input = prompt('Select Q | R | N | B');
+            input = input.toUpperCase();
+            while (true) {
+                if (
+                    input === 'Q' || input === 'R'
+                    || input === 'B' || input === 'N'
+                ) break;
+                else {
+                    input = prompt('Wrong input. Select Q | R | N | B');
+                }
+            }
+            if (input === 'Q') 
+                temp[row][col] = new Queen(board[selectedRow][selectedCol].color, row, col);
+            if (input === 'R') 
+                temp[row][col] = new Rook(board[selectedRow][selectedCol].color, row, col);
+            if (input === 'B') 
+                temp[row][col] = new Bishop(board[selectedRow][selectedCol].color, row, col);
+            if (input === 'N') 
+                temp[row][col] = new Knight(board[selectedRow][selectedCol].color, row, col);
+        } else {
+            temp[row][col].row = row;
+            temp[row][col].col = col;
+        }
             
         temp[selectedRow][selectedCol] = null;
         temp[row][col].moves++;
